a43e54c026cca847a92ae2a740d56bdb0a78172c
RupeWard <rupe@rupertward.co.uk>
2016-12-12 17:53:37 +0000

    DebugDescribe for collections

--- PythagorasUnity/Assets/Scripts/Core/IDebugDescribable.cs
+++ PythagorasUnity/Assets/Scripts/Core/IDebugDescribable.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace RJWard.Core
 {
@@ -6,18 +8,6 @@ namespace RJWard.Core
 	{
 		void DebugDescribe( System.Text.StringBuilder sb );
 	}
-
-	/*
-	public static class DebugDescribeExtensions
-	{
-		private static System.Text.StringBuilder sb = new System.Text.StringBuilder( );
-		public static string DebugDescribe(this IDebugDescribable obj)
-		{
-			sb.Length = 0;
-			obj.DebugDescribe( sb );
-			return sb.ToString( );
-		}
-	}*/
 }
 
 public static class DebugDescribeExtensions
@@ -35,5 +25,23 @@ public static class DebugDescribeExtensions
 		sb.Append( (dd == null) ? ("NULL") : (dd.DebugDescribe( )) );
 	}
 
+	public static void DebugDescribe< T >(this IEnumerable<T> coll, System.Text.StringBuilder sb)
+	{
+		sb.Append( "(" );
+		bool first = true;
+		foreach (T t in coll)
+		{
+			if (first)
+			{
+				first = false;
+			}
+			else
+			{
+				sb.Append( ", " );
+			}
+			sb.Append( t.ToString( ) );			
+		}
+		sb.Append( ")" );
+	}
 }
 
